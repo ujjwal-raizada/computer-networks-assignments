f47a0accc9fc3a929c5b1d6f0db9e76a9c12239e
Ujjwal Raizada <ujjwalraizada@gmail.com>
2020-04-07 08:19:51 +0530

    assn-3: Add file transfer over protocol

---
+++ assn-3/rdt-over-udp-python/file transfer.py
@@ -0,0 +1,48 @@
+from rdt import RDT
+
+port1 = 2000
+port2 = 3000
+
+def server():
+    socket = RDT('localhost', port1)
+    socket.connect('localhost', port2)
+    socket.listen()
+
+    filename = 'test_image.jpeg'
+    f = open(filename, 'rb')
+    l = f.read(100)
+    while(l):
+        
+        if (type(l) == str):
+            break
+
+        l = l.decode()
+        socket.send(l)
+        print("Sent: ", l)
+        l = f.read(512)
+    f.close()
+
+    print('done sending')
+
+
+def client():
+    socket = RDT('localhost', port2)
+    socket.connect('localhost', port1)
+    socket.listen()
+
+    with open('recieved_file.jpeg', 'wb') as f:
+        while True:
+            data = socket.recv()
+            data = data.encode()
+            print("recv data: ", data)
+            if not data:
+                break
+            f.write(data)
+    print("transfer complete")
+
+t = input("type: ")
+if (t == 'server'):
+    server()
+else:
+    client()
+
